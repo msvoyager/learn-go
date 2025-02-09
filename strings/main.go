938848bc9820b073a4f402ae5480819833b95c66
MS05-Voyager <minudasaswitha05@gmail.com>
2025-02-10 00:35:28 +0530

    updated strings file

--- strings/main.go
+++ strings/main.go
@@ -12,9 +12,9 @@ func main() {
 
 	// but we can refer a variable to use it for other place holder with the refering number
 
-	fmt.Printf("%8T %[1]v\n", s) // telling that first argument wish is s to use 
-	fmt.Printf("%v is hello and %[2]v is %[1]v\n",s,b)
-	//so we tell that [1]v is 1st argument which is s 
+	fmt.Printf("%8T %[1]v\n", s) // telling that first argument wish is s to use
+	fmt.Printf("%v is hello and %[2]v is %[1]v\n", s, b)
+	//so we tell that [1]v is 1st argument which is s
 	//and [2]v refer to the 2nd argument which is b
 
 	//we can get the 32 bit unicode code point using rune
@@ -28,11 +28,13 @@ func main() {
 	fmt.Printf("%8T %[1]v\n", []byte(s))
 	//output ->  []uint8 [78 199 144 32 104 199 142 111]
 	//so in this case instead of large number for over sized char it used two integers for that
-	//instead 464(ǐ) this use 99 144 both 
+	//instead 464(ǐ) this use 99 144 both
 
-	
+	a := []byte(s)
 
+	fmt.Printf("%d\n", len(a))
+	fmt.Printf("%d\n", len(s))
 
-
+	//we can only see 6 charac in "Nǐ hǎo" with the space but len return the length of the byte string that need to encode this in utf-8
 
 }
