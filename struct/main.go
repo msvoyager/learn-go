fe650864d9cdafdbbf8d4a32eb217822c98c7304
MS05-Voyager <minudasaswitha05@gmail.com>
2025-04-07 09:03:31 +0530

    struct embedding and use pointers with struct

--- struct/main.go
+++ struct/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type AuditInfo struct {
+	CreatedAt time.Time
+	LastModified time.Time
+}
 
 type BankAcc struct {
 	AccName string
 	Balance float64
+	AuditInfo //embeddingaudiinfo struct 
 	
+}
+type Employee struct {
+
+	// taking variables
+	name string
+	empid int
 }
 func main() {
 	account := BankAcc{AccName: "minuda", Balance: 5000.00}
@@ -15,11 +28,27 @@ func main() {
 
 	account2 := &BankAcc{AccName: "saswitha", Balance: 7500.00}
 	fmt.Println(account2)
-	fmt.Println(&account2)
-	account3 := &account2
-	fmt.Println(&account3)
-	fmt.Println(*account3)
-	*account3 = &BankAcc{AccName: "saswitha", Balance: 800.00}
-	fmt.Println(account2)
+
+	account3 := BankAcc{
+		AccName: "H.D.Minuda",
+		Balance: 1000.20,
+		AuditInfo: AuditInfo{CreatedAt: time.Now(), LastModified: time.Now()},
+	}
+
+	fmt.Println(account3)
+
+	emp := Employee{"ABC", 19078}
+	pts := &emp
+	fmt.Println(pts)
+
+	// accessing the struct fields(liem employee's name)
+	// using a pointer but here we are not using
+	// dereferencing explicitly
+	fmt.Println(pts.name)
+
+	// same as above by explicitly using
+	// dereferencing concept
+	// means the result will be the same
+	fmt.Println((*pts).name)
 
 }
