6137c5b6606c638d558becacedf55e2ec3c7b2eb
MS05-Voyager <minudasaswitha05@gmail.com>
2025-04-19 23:39:06 +0530

    using interfaces  with same methods but defferent structs that full fill the interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -13,8 +13,19 @@ func (a Article) String() string {
 	return fmt.Sprintf("The %q article was written by %s.", a.Title, a.Author)
 }
 
+type Book struct {
+	Title  string
+	Author string
+	Pages  int
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf("The %q book was written by %s.", b.Title, b.Author)
+}
+//so no we have two structs and two methods 
 type Stringer interface {
 	String() string
+
 }
 
 func main() {
@@ -23,8 +34,22 @@ func main() {
 		Author: "Sammy Shark",
 	}
 	Print(a)
+
+	b := Book{
+		Title: "learn go by practice",
+		Author: "henry max",
+		Pages: 1000,
+	}
+	Print(b)
 }
+
+//without using interfaces we can create a function to call the method
+// func Print(a Article) {
+// 	fmt.Println(a.String())
+// }
+
 
 func Print(s Stringer) {
 	fmt.Println(s.String())
+
 }
